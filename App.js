fabe0cb96e7f957d3f5637a386cf2fdbbd4cdd43
Mohit Maroliya <mmaroliya@gmail.com>
2022-06-22 18:49:37 +0530

    Adding assets,homepage& details page

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
+import Home from './screens/Home';
+import Details from './screens/Details';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
+  const [loaded] = useFonts({
+    InterBold: require('./assets/fonts/Inter-Bold.ttf'),
+    InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+  });
+
+  if(!loaded) return null;
+
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      backround:"transparent"
+    }
+  }
+
   return (
-    <View >
-      <Text>This is the first APP</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName="Home">
+        <Stack.Screen name='Home' component={Home}></Stack.Screen>
+        <Stack.Screen name='Details' component={Details}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
