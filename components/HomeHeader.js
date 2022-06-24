85f57106e27e6e303b3e691a7a02e167da25757b
Mohit Maroliya <mmaroliya@gmail.com>
2022-06-24 16:52:11 +0530

    Adding the HomeHeader/ListHeader component

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput } from 'react-native';
+import { COLORS, SIZES, FONTS, assets } from '../constants';
 
-const HomeHeader = () => {
+
+const HomeHeader = ({onSearch}) => {
   return (
-    <View>
-      <Text>HomeHeader</Text>
+    <View style={{backgroundColor:COLORS.primary, padding:SIZES.font}}>
+      <View style={{flexDirection:'row',justifyContent:'space-between', alignItems:'center'}}>
+        <Image source={assets.logo} resizeMode="contain" style={{width:90, height:25}} />
+        <View style={{width:45, height:45}}>
+          <Image source={assets.person01} resizeMode="contain" style={{width:'100%' , height:'100%'}} />
+          <Image source={assets.badge} resizeMode="contain" style={{width:19 , height:19, position:'absolute', bottom:0, right:0}} />
+        </View>
+      </View>
+
+      <View style={{marginVertical:SIZES.font}}>
+        <Text style={{fontSize:SIZES.small, color:COLORS.white}}>Hello Mohit!</Text>
+        <Text style={{fontSize:SIZES.large, fontFamily:FONTS.bold , color:COLORS.white, marginTop:SIZES.base / 2}}>Lets Find Our NFT</Text>
+      </View>
+
+      <View style={{marginTop:SIZES.font}}>
+        <View style={{width:'100%', borderRadius:SIZES.font, backgroundColor:COLORS.secondary, flexDirection:'row',alignItems:'center', paddingHorizontal:SIZES.font, padding:SIZES.small-2}}>
+          <Image source={assets.search} resizeMode="contain" style={{height:20, width:20, marginRight:SIZES.base}} />
+          <TextInput placeholder='Search NFTs' style={{flex:1,color:COLORS.white}} onChangeText={onSearch} />
+        </View>
+      </View>
     </View>
   )
 }
